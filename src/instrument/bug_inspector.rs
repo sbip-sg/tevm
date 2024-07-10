759a9f9249b0827c165b63e83cfcc1f89a6bc8da
chen <chencassc@gmail.com>
2024-07-10 17:40:58 +0800

    adding bug data in instruction step

--- src/instrument/bug_inspector.rs
+++ src/instrument/bug_inspector.rs
@@ -26,6 +26,8 @@ pub struct BugInspector {
     pub opcode_index: usize,
     /// Stack inputs of the current opcodes. Only updated when the opcode is interesting
     inputs: Vec<U256>,
+    /// Current opcode
+    opcode: u8,
 }
 
 impl BugInspector {
@@ -58,7 +60,7 @@ impl BugInspector {
 
     /// Record the program counter for the given contract address
     pub fn record_pc(&mut self, address: Address, pc: usize) {
-        let pcs = self.pcs_by_address.entry(address).or_insert(HashSet::new());
+        let pcs = self.pcs_by_address.entry(address).or_default();
         pcs.insert(pc);
     }
 
@@ -107,15 +109,29 @@ where
     fn step(&mut self, interp: &mut Interpreter, context: &mut EvmContext<DB>) {
         let _ = interp;
         let _ = context;
-        let opcode = OpCode::new(interp.current_opcode());
-        match opcode {
-            Some(OpCode::ADD | OpCode::SUB | OpCode::MUL | OpCode::DIV | OpCode::SDIV) => {
-                let a = interp.stack().peek(0).unwrap();
-                let b = interp.stack().peek(1).unwrap();
+        self.opcode = interp.current_opcode();
+        let opcode = OpCode::new(self.opcode);
+
+        // it's possible to handle REVERT, INVALID here
+        if let Some(
+            OpCode::ADD
+            | OpCode::SUB
+            | OpCode::MUL
+            | OpCode::DIV
+            | OpCode::SDIV
+            | OpCode::SMOD
+            | OpCode::ADDMOD
+            | OpCode::MULMOD
+            | OpCode::EXP,
+        ) = opcode
+        {
+            self.inputs.clear();
+            let a = interp.stack().peek(0);
+            let b = interp.stack().peek(1);
+            if let (Ok(a), Ok(b)) = (a, b) {
                 self.inputs.push(a);
                 self.inputs.push(b);
             }
-            _ => {}
         }
     }
 
@@ -124,40 +140,89 @@ where
         let address = interp.contract().target_address;
         let address_index = self.record_seen_address(address);
         let pc = interp.program_counter();
-        let opcode = interp.current_opcode();
+        let opcode = self.opcode;
 
         if self.instrument_config.pcs_by_address {
-            self.record_pc(address.into(), pc);
+            self.record_pc(address, pc);
         }
 
-        // Check for revert or invalid opcode
-        match &interp.instruction_result {
-            InstructionResult::Revert | InstructionResult::InvalidEFOpcode => {
+        let success = interp.instruction_result.is_ok();
+
+        // Check for overflow and underflow
+        match (opcode, success) {
+            (opcode::ADD, true) => {
+                if let Ok(r) = interp.stack().peek(0) {
+                    if let (Some(a), Some(b)) = (self.inputs.first(), self.inputs.get(1)) {
+                        if r < *a || r < *b {
+                            let bug = Bug::new(BugType::IntegerOverflow, opcode, pc, address_index);
+                            self.add_bug(bug);
+                        }
+                    }
+                }
+            }
+            (opcode::MUL, true) => {
+                if let (Some(a), Some(b)) = (self.inputs.first(), self.inputs.get(1)) {
+                    if mul_overflow(*a, *b) {
+                        let bug = Bug::new(BugType::IntegerOverflow, opcode, pc, address_index);
+                        self.add_bug(bug);
+                    }
+                }
+            }
+            (opcode::SUB, true) => {
+                if let (Some(a), Some(b)) = (self.inputs.first(), self.inputs.get(1)) {
+                    if a < b {
+                        let bug = Bug::new(BugType::IntegerSubUnderflow, opcode, pc, address_index);
+                        self.add_bug(bug);
+                    }
+                }
+            }
+            (opcode::DIV | opcode::SDIV | opcode::SMOD, true) => {
+                if let Some(b) = self.inputs.get(1) {
+                    if b == &U256::ZERO {
+                        let bug = Bug::new(BugType::IntegerDivByZero, opcode, pc, address_index);
+                        self.add_bug(bug);
+                    }
+                }
+            }
+            (opcode::ADDMOD | opcode::MULMOD, true) => {
+                if let Some(n) = self.inputs.get(2) {
+                    if n == &U256::ZERO {
+                        let bug = Bug::new(BugType::IntegerDivByZero, opcode, pc, address_index);
+                        self.add_bug(bug);
+                    }
+                }
+            }
+            (opcode::EXP, true) => {
+                // todo_cl check for overflow
+            }
+            (opcode::BLOBHASH, _) => {
+                let bug = Bug::new(BugType::BlockValueDependency, opcode, pc, address_index);
+                self.add_bug(bug);
+            }
+            (opcode::COINBASE, _) => {
+                let bug = Bug::new(BugType::BlockValueDependency, opcode, pc, address_index);
+                self.add_bug(bug);
+            }
+            (opcode::TIMESTAMP, _) => {
+                let bug = Bug::new(BugType::TimestampDependency, opcode, pc, address_index);
+                self.add_bug(bug);
+            }
+            (opcode::NUMBER, _) => {
+                let bug = Bug::new(BugType::BlockNumberDependency, opcode, pc, address_index);
+                self.add_bug(bug);
+            }
+            (opcode::DIFFICULTY, _) => {
+                let bug = Bug::new(BugType::BlockValueDependency, opcode, pc, address_index);
+                self.add_bug(bug);
+            }
+            (opcode::REVERT | opcode::INVALID, _) => {
                 let bug = Bug::new(BugType::RevertOrInvalid, opcode, pc, address_index);
                 self.add_bug(bug);
             }
-            _ => {}
-        }
 
-        // Check for overflow and underflow
-        match opcode {
-            opcode::ADD => {
-                // todo_cl
-            }
-            _ => {}
+            _ => (),
         }
     }
-
-    // #[inline]
-    // fn call_end(
-    //     &mut self,
-    //     context: &mut EvmContext<DB>,
-    //     inputs: &CallInputs,
-    //     outcome: CallOutcome,
-    // ) -> CallOutcome {
-
-    //     outcome
-    // }
 
     #[inline]
     fn create_end(
@@ -173,5 +238,14 @@ where
             }
         }
         CreateOutcome::new(result, address)
+    }
+}
+
+fn mul_overflow(a: U256, b: U256) -> bool {
+    let zero = U256::ZERO;
+    if a == zero || b == zero {
+        false
+    } else {
+        a > U256::MAX.wrapping_div(b)
     }
 }
